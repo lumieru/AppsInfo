118c79a7fb682a853843a39ca2fc178e23da3907
lubin <lumieru@gmail.com>
2014-05-17 21:21:20 +0800

    判断条件写错了

--- cloud/app.js
+++ cloud/app.js
@@ -16,29 +16,32 @@ app.get('/link3', function(req, res) {
 	
 	var type = REDIRECT;
 	var link;
+	var browser;
 	var userAgent = req.headers['user-agent'].toLowerCase();
 	if (userAgent.indexOf('android') > -1) {
-		if (userAgent.indexOf('weibo')) {
+		if (userAgent.indexOf('weibo') > -1) {
 			//android版微博
 			type = OPEN_IN_BROWSER;
-		} else if(userAgent.indexOf('micromessenger')) {
+			browser = '浏览器打开';
+		} else if(userAgent.indexOf('micromessenger') > -1) {
 			//android版微信
 			type = COPY_AND_OPEN_IN_BRWOSER;
-		} else if(userAgent.indexOf('qq')) {
+		} else if(userAgent.indexOf('qq') > -1) {
 			//android版qq
 			type = OPEN_IN_BROWSER;
+			browser = '用浏览器打开';
 		}  
 		else {
 			link = 'market://details?id=com.tencent.mm';
 		}
 	} else if (userAgent.indexOf('iphone') > -1) {
-		if(userAgent.indexOf('micromessenger')) {
+		if(userAgent.indexOf('micromessenger') > -1) {
 			type = FAIL_TO_REDIRECT;
 		} else {
 			link = 'https://itunes.apple.com/app/id440650698';
 		}
 	} else if (userAgent.indexOf('ipad') > -1) {
-		if(userAgent.indexOf('micromessenger')) {
+		if(userAgent.indexOf('micromessenger') > -1) {
 			type = FAIL_TO_REDIRECT;
 		} else {
 			link = 'https://itunes.apple.com/app/id443871575';
@@ -49,14 +52,17 @@ app.get('/link3', function(req, res) {
 	
 	if(type == REDIRECT) {
 		res.writeHead(301, {"Location": link});
-	} else if(type == OPEN_IN_BROWSER) {
-		res.write('<html><title>下载《虚拟乒乓球》</title><body><label><p>请点击<span style="color:red">右上角的按钮</span></p></label><label><p>选择<span style="color:red">「在浏览器中打开」</span>即可正常下载</p></label></body></html>');
-	} else if(type == COPY_AND_OPEN_IN_BRWOSER) {
-		res.write('<html><title>下载《虚拟乒乓球》</title><body>请复制这个链接' + req.url + '，并用安卓的系统浏览器打开它，就能完成下载。</body></html>');
 	} else {
-		//FAIL_TO_REDIRECT
-		res.write('<html><title>下载《虚拟乒乓球》</title><body>由于浏览器不能自动跳转，请点击<a href="' + link + '">《虚拟乒乓球》</a>来下载。</body></html>');
-	}	
+		res.setHeader("Content-Type", "text/html; charset=utf-8");
+		if(type == OPEN_IN_BROWSER) {
+			res.write('<html><title>虚拟乒乓球</title><body><label><p>请点击<span style="color:red">右上角的按钮</span></p></label><label><p>选择<span style="color:red">「' + browser + '」</span>即可正常下载</p></label></body></html>');
+		} else if(type == COPY_AND_OPEN_IN_BRWOSER) {
+			res.write('<html><title>虚拟乒乓球</title><body>请复制这个链接' + req.url + '，并用安卓的系统浏览器打开它，就能完成下载。</body></html>');
+		} else {
+			//FAIL_TO_REDIRECT
+			res.write('<html><title>虚拟乒乓球</title><body>由于浏览器不能自动跳转，请点击<a href="' + link + '">《虚拟乒乓球》</a>来下载。</body></html>');
+		}	
+	}
    	res.end();
 });
 
