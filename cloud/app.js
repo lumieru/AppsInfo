7acdd1f51efec0f9f852407728056b628b2ac22f
lubin <lumieru@gmail.com>
2014-05-17 16:23:45 +0800

    根据不同的客户端，返回不同的vtt下载链接

--- cloud/app.js
+++ cloud/app.js
@@ -12,5 +12,23 @@ app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
+// 根据不同的客户端，返回不同的vtt下载链接
+app.get('/link', function(req, res) {
+	var link;
+	var userAgent = req.headers['user-agent'].toLowerCase();
+	if (userAgent.indexOf('android') > -1) {
+		link = 'market://details?id=com.tencent.mm';
+	} else if (userAgent.indexOf('iphone') > -1) {
+		link = 'http://t.cn/8sfohxY';
+	} else if (userAgent.indexOf('ipad') > -1) {
+		link = 'http://t.cn/8sfo20j';
+	} else {
+		link = 'http://www.sensedevil.com';
+	}
+	
+	res.writeHead(301, {"Location": link});
+    res.end();
+});
+
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
