501dc2f6815b68938e174f86fee1fa7c125307a8
lubin <lumieru@gmail.com>
2014-05-17 16:39:04 +0800

    去掉没用的url入口

--- cloud/app.js
+++ cloud/app.js
@@ -7,11 +7,6 @@ app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
-// 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
-app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
-});
-
 // 根据不同的客户端，返回不同的vtt下载链接
 app.get('/link', function(req, res) {
 	var link;
